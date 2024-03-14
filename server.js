025328cc6ea336e6eb773b27e450ed954e655fa4
Oliver Temple <oliver.temple.dev@gmail.com>
2024-03-14 15:37:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.json()) //This parses incoming requests as JSON payloads
 
 //You need to ensure that you have run 'ionic build' in the front end repo and that this relative path is correct for your system for this to work.
-app.use(express.static(path.resolve(__dirname, "../EarthquakeFrontend/earthquake-frontend/www"))) //This is to serve the ionic page
+app.use(express.static(path.resolve(__dirname, "./frontend_build/www"))) //This is to serve the ionic page
 
 app.get("/api", (req, res) => {
     res.json({key: "value"})
