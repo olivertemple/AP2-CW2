5583e21b69d0d31fdfc2b3057b3c06d127d0b758
Oliver Temple <Oliver.Temple@dyson.com>
2024-03-19 12:27:12 +0000

    added router for the api

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 
 const port = 3000;
 
+const earthquakeRoutes = require("./routes/earthquake");
+
 app.use(express.json()) //This parses incoming requests as JSON payloads
 
-//You need to ensure that you have run 'ionic build' in the front end repo and that this relative path is correct for your system for this to work.
+//If there is no build in the ./frontend_build/ folder then this won't work. You can download the latest stable build from github
 app.use(express.static(path.resolve(__dirname, "./frontend_build/www"))) //This is to serve the ionic page
 
-app.get("/api", (req, res) => {
-    res.json({key: "value"})
-})
+app.use("/api/earthquake", earthquakeRoutes);
 
 app.listen(port, (err) => {
     err ? console.log(err) : console.log(`Listening on port ${port}`)
