eceb868816872fb6774fab8889e052c6eb499549
Oliver Temple <oliver.temple.dev@gmail.com>
2024-04-22 13:44:13 +0100

    added a connection timeout of 200ms for initial connection to check the server can be reached (#5)
    Co-authored-by: Oliver Temple <Oliver.Temple@dyson.com>

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const sql = require("mssql");
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express();
 
 const port = 3000;
 
@@ -11,23 +13,39 @@ const samplesRoutes = require("./routes/samples");
 const usersRoutes = require("./routes/users");
 const exampleRoutes = require("./routes/example_endpoints");
 
+var config = {
+    "user": process.env.USER, // Database username
+    "password": process.env.PASSWORD, // Database password
+    "server": process.env.SERVER, // Server IP address
+    "database": process.env.DATABASE, // Database name
+    "options": {
+        "encrypt": false, // Disable encryption
+        "trustServerCertificate": false
+    },
+    "connectionTimeout": 200
+}
 
-app.use(express.json()) //This parses incoming requests as JSON payloads
+sql.connect(config, async err => {
+    if (err){
+        console.log("Cannot connect to sql server")
+    }else{
+        const app = express();
+        app.use((req,res,next) => {
+            res.setHeader("Access-Control-Allow-Origin", "*")
+            next();
+        })
+        app.use(express.json()) //This parses incoming requests as JSON payloads
 
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    next();
+        //If there is no build in the ./frontend_build/ folder then this won't work. You can download the latest stable build from github
+        app.use(express.static(path.resolve(__dirname, "./frontend_build/www"))) //This is to serve the ionic page
+
+        app.use("/api/earthquake", earthquakeRoutes);
+        app.use("/api/observatory", observatoryRoutes);
+        app.use("/api/samples", samplesRoutes);
+        app.use("/api/users", usersRoutes);
+        app.use("/api/examples", exampleRoutes);
+        app.listen(port, (err) => {
+            err ? console.log(err) : console.log(`Listening on port ${port}`)
+        });
+    }
 })
-
-//If there is no build in the ./frontend_build/ folder then this won't work. You can download the latest stable build from github
-app.use(express.static(path.resolve(__dirname, "./frontend_build/www"))) //This is to serve the ionic page
-
-app.use("/api/earthquake", earthquakeRoutes);
-app.use("/api/observatory", observatoryRoutes);
-app.use("/api/samples", samplesRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/examples", exampleRoutes);
-
-app.listen(port, (err) => {
-    err ? console.log(err) : console.log(`Listening on port ${port}`)
-});
--- utils/services.js
+++ utils/services.js
@@ -1,7 +1,15 @@
 function check_body_schema(body, schema) {
     for (let attribute in schema) {
-        if (schema[attribute][1] && typeof body[attribute] !== schema[attribute][0]) {
-            return false;
+        if (schema[attribute][1]){
+            if (typeof body[attribute] !== schema[attribute][0]) {
+                return false;
+            }
+        } else {
+            if (body[attribute]){
+                if (typeof body[attribute] !== schema[attribute][0]){
+                    return false
+                }
+            }
         }
     }
     return true;
