5c8a6a3583cd37dad5fccf9da2c8ee0a7596b785
Oliver Temple <Oliver.Temple@dyson.com>
2024-05-06 16:57:42 +0100

    mark as collected and send email

--- routes/transactions.js
+++ routes/transactions.js
@@ -53,6 +53,8 @@ router.post("/add_transaction", (req, res) => {
                 ${req.body.sample_id}
             )`).then(async _ => {
                 try{
+                    sql.query(`UPDATE SampleData SET IsSold = 1 WHERE SampleID = ${req.body.sample_id}`)
+
                     let email_req = await sql.query(`SELECT email, username FROM users WHERE user_id = ${req.body.buyer_id}`);
                     let email = email_req.recordset[0].email;
                     let username = email_req.recordset[0].username;
@@ -62,17 +64,22 @@ router.post("/add_transaction", (req, res) => {
     
                     let earthquake_req = await sql.query(`SELECT * FROM EarthquakeData WHERE Id = ${sample.EarthquakeId}`)
                     let earthquake = earthquake_req.recordset[0];
+
+                    let order_req = await sql.query(`SELECT * FROM Transactions WHERE BuyerId = '${req.body.buyer_id}' AND SampleId = '${req.body.sample_id}';`)
+                    let order = order_req.recordset.slice(-1)[0];
+
                     sendMailOrderConfimation(
                         email,
                         username,
-                        1,
+                        order.TransactionId,
                         sample.EarthquakeId,
                         earthquake,
                         sample.Observations,
                         sample.ItemValue,
                         sample.ItemValue,
                         req.body.date_of_purchase,
-                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTe3hKH5U9hzPmkWe9XwVn1Kx0UGI4a54UFk7GYM3x4w&s"
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTe3hKH5U9hzPmkWe9XwVn1Kx0UGI4a54UFk7GYM3x4w&s",
+                        "confirmation"
                     )
                     res.status(200).send();
                     return true;
@@ -89,7 +96,63 @@ router.post("/add_transaction", (req, res) => {
     })
 })
 
-router.post("/update_transaction", (req, res) => {
 
+router.get("/transaction_collected", (req, res) => {
+    if (!req.query.id) {
+        res.status(400).send("no id sent");
+        return false;
+    }
+
+    if (!parseInt(req.query.id, 10)) {
+        res.status(400).send("invalid id");
+        return false;
+    }
+
+    let transaction_id = req.query.id;
+
+    sql.connect(config, async err => {
+        if (err) {
+            res.status(500).send(err);
+            return false;
+        }
+
+        try {
+            sql.query(`UPDATE Transactions SET collection_status='collected' WHERE TransactionId = ${transaction_id};`)
+
+            let sql_res = await sql.query(`SELECT * FROM Transactions WHERE TransactionId = ${transaction_id};`)
+            let transaction = sql_res.recordset.slice(-1)[0];
+
+            let sample_res = await sql.query(`SELECT * FROM SampleData WHERE SampleId = ${transaction.SampleId}`)
+            let sample = sample_res.recordset[0];
+
+            let earthquake_res = await sql.query(`SELECT * FROM EarthquakeData WHERE Id = ${sample.EarthquakeId}`)
+            let earthquake = earthquake_res.recordset[0];
+
+            let user_res = await sql.query(`SELECT * FROM users WHERE user_id = ${transaction.BuyerId}`)
+            let user = user_res.recordset[0];
+
+            sendMailOrderConfimation(
+                user.email,
+                user.username,
+                transaction.TransactionId,
+                sample.EarthquakeId,
+                earthquake,
+                sample.Observations,
+                sample.ItemValue,
+                sample.ItemValue,
+                transaction.date_of_purchase,
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTe3hKH5U9hzPmkWe9XwVn1Kx0UGI4a54UFk7GYM3x4w&s",
+                "collection"
+            )
+
+            res.status(200).send();
+
+
+        } catch (err) {
+            res.status(500).send(err);
+            return false;
+        }
+
+    })
 })
 module.exports = router;
--- utils/email.js
+++ utils/email.js
@@ -1,10 +1,10 @@
-async function sendMailOrderConfimation(email, user_name, order_number, description, earthquake_info, sample_description, price, total, order_date, image_url) {
+async function sendMailOrderConfimation(email, user_name, order_number, description, earthquake_info, sample_description, price, total, order_date, image_url, template) {
     try{
         let res = await fetch("https://lmuoyu2qlf.execute-api.eu-central-1.amazonaws.com/default/ap2_cw2_emailsend", {
             method: "POST",
             body: JSON.stringify({
                 email: email,
-                subject: "Order Confirmation!",
+                subject: template == "confirmation" ? "Order Confirmation!" : "Order Collected!",
                 user_name: user_name,
                 order_number: order_number,
                 description: description,
@@ -13,7 +13,8 @@ async function sendMailOrderConfimation(email, user_name, order_number, descript
                 total: total,
                 order_date: order_date,
                 image: image_url,
-                earthquake_info: earthquake_info
+                earthquake_info: earthquake_info,
+                template: template
             })
         });
 
