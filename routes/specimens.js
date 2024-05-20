8cb40092b1c74931f32adfc22f317cfd54d444d6
Oliver Temple <oliver.temple.dev@gmail.com>
2024-05-20 08:35:10 +0100

    Location (#40)
    * return location on sample creation
    
    * fix for optional sample location
    
    ---------
    
    Co-authored-by: Oliver Temple <Oliver.Temple@dyson.com>

--- routes/specimens.js
+++ routes/specimens.js
@@ -74,7 +74,9 @@ const empty_item_name = ''
 
 router.post("/create", async (req, res) => {
     
-
+    if(req.body?.current_location == null){
+        req.body.current_location = ""
+    }
     let errors = check_body_schema(req.body, createBodySchema);
     if (errors.length > 0) {
         res.status(400).json({message: "Invalid request body", errors: errors});
@@ -95,7 +97,7 @@ router.post("/create", async (req, res) => {
             for (i = 65; i <= 90; i++) {
                 letter = String.fromCharCode(i);
                 for (num = 0; num <= 9; num++) {
-                    shelf = letter + String(num)
+                    shelf = letter + String(num);
                     sql_query = `SELECT COUNT(*) FROM SampleData WHERE current_location = '${shelf}'`
                     var storage_check = await sql.query(sql_query)
                     const storage_recordset = storage_check.recordset;
@@ -146,7 +148,7 @@ router.post("/create", async (req, res) => {
                     '${image_url}',
                     '${empty_item_name}'
                 )`).then(async _ => {
-                    res.status(200).json({message:"Specimen added"});
+                    res.status(200).json({message: `please store it on shelf ${location}`});
                     return true;
                 }).catch(err => {
                     res.status(500).json({message: "Could not add specimen", errors: err});
